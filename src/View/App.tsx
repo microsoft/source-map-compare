b366b8495c15ca37b9be364d450fbd44af3c9e32
Daniel Chattan <danieln@microsoft.com>
2024-04-25 08:38:02 -0400

    add react hooks lint rules

--- src/View/App.tsx
+++ src/View/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { AppArguments } from '../AppArguments';
+import type { AppArguments, BundleComparisonAppArgs } from '../AppArguments';
 import { useCompareModel, useExploreModel } from '../Layouts';
 import { BundleView } from '../Components';
 
@@ -7,15 +7,23 @@ export interface AppProps {
   args: AppArguments;
 }
 
+function BrowseBundle(args: AppArguments) {
+  const { columns, items } = useExploreModel(args.bundles);
+  return <BundleView columns={columns} items={items} />;
+}
+
+function CompareBundles(args: BundleComparisonAppArgs) {
+  const { columns, items } = useCompareModel(args.baseline, args.compare);
+  return <BundleView columns={columns} items={items} />;
+}
+
 export const App: React.FC<AppProps> = ({ args }) => {
   switch (args.mode) {
     case 'comparison': {
-      const { columns, items } = useCompareModel(args.baseline, args.compare);
-      return <BundleView columns={columns} items={items} />;
+      return <CompareBundles {...args} />;
     }
     case 'single': {
-      const { columns, items } = useExploreModel(args.bundles);
-      return <BundleView columns={columns} items={items} />;
+      return <BrowseBundle {...args} />;
     }
   }
 };
